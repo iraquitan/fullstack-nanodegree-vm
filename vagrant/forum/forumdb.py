94f93d05d022bed8479afbf49c12a96d5cf71d9b
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2015-10-28 08:28:42 -0300

    feat: Changed to work with Postgresql

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -3,29 +3,46 @@
 # 
 
 import time
+import bleach
+import psycopg2
 
-## Database connection
-DB = []
+# Database connection
+# DB = []
 
-## Get posts from database.
-def GetAllPosts():
-    '''Get all the posts from the database, sorted with the newest first.
+
+# Get posts from database.
+def get_all_posts():
+    """Get all the posts from the database, sorted with the newest first.
 
     Returns:
       A list of dictionaries, where each dictionary has a 'content' key
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
-    '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    """
+    db = psycopg2.connect('dbname=forum')
+    c = db.cursor()
+    query = "SELECT time, content FROM posts ORDER BY time DESC;"
+    c.execute(query)
+    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in
+             c.fetchall()]
+    db.close()
+    # posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
+    # posts.sort(key=lambda row: row['time'], reverse=True)
     return posts
 
-## Add a post to the database.
-def AddPost(content):
-    '''Add a new post to the database.
+
+# Add a post to the database.
+def add_post(content):
+    """Add a new post to the database.
 
     Args:
       content: The text content of the new post.
-    '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    """
+    db = psycopg2.connect('dbname=forum')
+    c = db.cursor()
+    query = "INSERT INTO posts (content) VALUES (%s);"
+    c.execute(query, (bleach.clean(content),))  # Secure query execute
+    db.commit()
+    db.close()
+    # t = time.strftime('%c', time.localtime())
+    # DB.append((t, content))
