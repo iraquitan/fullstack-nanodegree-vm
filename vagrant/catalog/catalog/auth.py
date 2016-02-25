e8b89e72b0c363f73e9f8a6a2be45d9ec60afa55
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2016-02-25 18:42:37 -0300

    feat: Add abstract OauthSingIn class and Google and Facebook sign in oauth classes

---
+++ vagrant/catalog/catalog/auth.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+ * Created by PyCharm.
+ * Project: catalog
+ * Author name: Iraquitan Cordeiro Filho
+ * Author login: pma007
+ * File: auth.py
+ * Date: 2/25/16
+ * Time: 11:46
+ * To change this template use File | Settings | File Templates.
+"""
+import json
+import urllib2
+
+from flask import url_for, current_app, request, redirect
+from rauth import OAuth2Service
+
+
+class OAuthSignIn(object):
+    providers = None
+
+    def __init__(self, provider_name):
+        self.provider_name = provider_name
+        credentials = current_app.config['OAUTH_CREDENTIALS'][provider_name]
+        self.consumer_id = credentials['id']
+        self.consumer_secret = credentials['secret']
+
+    def authorize(self):
+        pass
+
+    def callback(self):
+        pass
+
+    def get_callback_url(self):
+        return url_for('home.oauth_callback', provider=self.provider_name,
+                       _external=True)
+
+    @classmethod
+    def get_provider(cls, provider_name):
+        if cls.providers is None:
+            cls.providers = {}
+            for provider_class in cls.__subclasses__():
+                provider = provider_class()
+                cls.providers[provider.provider_name] = provider
+        return cls.providers[provider_name]
+
+
+class GoogleSignIn(OAuthSignIn):
+    def __init__(self):
+        super(GoogleSignIn, self).__init__('google')
+        googleinfo = urllib2.urlopen(
+            'https://accounts.google.com/.well-known/openid-configuration')
+        google_params = json.load(googleinfo)
+        self.service = OAuth2Service(
+                name='google',
+                client_id=self.consumer_id,
+                client_secret=self.consumer_secret,
+                authorize_url=google_params.get('authorization_endpoint'),
+                access_token_url=google_params.get('token_endpoint'),
+                base_url=google_params.get('userinfo_endpoint')
+        )
+
+    def authorize(self):
+        return redirect(self.service.get_authorize_url(
+            scope='email profile',
+            response_type='code',
+            redirect_uri=self.get_callback_url())
+            )
+
+    def callback(self):
+        if 'code' not in request.args:
+            return None, None, None, None
+        oauth_session = self.service.get_auth_session(
+                data={'code': request.args['code'],
+                      'grant_type': 'authorization_code',
+                      'redirect_uri': self.get_callback_url()
+                      },
+                decoder=json.loads
+        )
+        me = oauth_session.get('').json()
+        return (
+            "google${}".format(me['sub']),
+            me['name'],
+            me['email'],
+            me['picture']
+        )
+
+
+class FacebookSignIn(OAuthSignIn):
+    def __init__(self):
+        super(FacebookSignIn, self).__init__('facebook')
+        self.service = OAuth2Service(
+            name='facebook',
+            client_id=self.consumer_id,
+            client_secret=self.consumer_secret,
+            authorize_url='https://graph.facebook.com/oauth/authorize',
+            access_token_url='https://graph.facebook.com/oauth/access_token',
+            base_url='https://graph.facebook.com/'
+        )
+
+    def authorize(self):
+        return redirect(self.service.get_authorize_url(
+            scope='email',
+            response_type='code',
+            redirect_uri=self.get_callback_url())
+        )
+
+    def callback(self):
+        if 'code' not in request.args:
+            return None, None, None, None
+        oauth_session = self.service.get_auth_session(
+            data={'code': request.args['code'],
+                  'grant_type': 'authorization_code',
+                  'redirect_uri': self.get_callback_url()}
+        )
+        me = oauth_session.get('me').json()
+        me_picture = oauth_session.get('me/picture', redirect=0,
+                                       height=200, width=200).json()
+        return (
+            "facebook${}".format(me['id']),
+            me.get('name'),
+            me.get('email'),
+            me_picture.get('data')['url']
+        )
